e73fa525e50813c05ab465c111754ede55e29d7e
h0s04qs <himanshu.sharma2@walmart.com>
2025-09-10 20:19:14 +0530

    Updating character qoutes

--- app/components/ui/friends-character.tsx
+++ app/components/ui/friends-character.tsx
@@ -262,7 +262,7 @@ export default function FriendsCharacter({
                 >
                   <div className="bg-white dark:bg-gray-800 rounded-lg p-3 shadow-lg border border-gray-200 dark:border-gray-600 relative">
                     <p className="text-sm font-medium text-gray-800 dark:text-gray-200">
-                      "{currentCharacter.quotes[currentQuoteIndex]}"
+                      {currentCharacter.quotes[currentQuoteIndex]}
                     </p>
                     
                     {/* Speech bubble arrow */}
